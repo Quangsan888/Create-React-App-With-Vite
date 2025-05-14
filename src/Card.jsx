77441f15aeefa0d524f722473d3c5a29e8227461
Quang-keri <phamquang040102@gmail.com>
2025-05-15 03:11:20 +0700

    push git thử

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+function Card() {
+  return (
+    <div className="card">
+      <div>
+        <img src="#" alt="" />
+        <h2>Quang san</h2>
+        <p>Học code react</p>
+        <p>rất mệt</p>
+      </div>
+    </div>
+  );
+}
+export default Card;
