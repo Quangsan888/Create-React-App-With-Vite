28ff86f5ff6c2e819abbc73a7b8729debc0f1dd2
Quang-keri <phamquang040102@gmail.com>
2025-05-15 14:07:19 +0700

    học về props truyền data vào component và định nghĩa propType

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Button from "./Button.jsx";
-import Card from "./Card.jsx";
+import Student from "./Student.jsx";
+import "./index.css";
 function App() {
   return (
     <div>
-      <Card />;
-      <Button />
+      <Student name="Quangsan" age={21} isStudent={true} />
+      <Student name="Nguyen" age={22} isStudent={false} />
+      <Student name="Quangsan" age={23} isStudent={false} />
+      <Student name="Quangsan" age={21} isStudent={true} />
+      <Student />
     </div>
   );
 }
